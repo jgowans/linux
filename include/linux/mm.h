8ca1a67ccba34fd53a76fd5cb644d2fd0d283064
Yulei Zhang <yuleixzhang@tencent.com>
2020-10-08 15:54:22 +0800

    mm, dmemfs: register and handle the dmem mce
    dmemfs register the mce handler, send signal to the procs
    whose vma is mapped in mce pfn.
    
    Signed-off-by: Haiwei Li <lihaiwei@tencent.com>
    Signed-off-by: Yulei Zhang <yuleixzhang@tencent.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3046,6 +3046,8 @@ extern int memory_failure(unsigned long pfn, int flags);
 extern void memory_failure_queue(unsigned long pfn, int flags);
 extern void memory_failure_queue_kick(int cpu);
 extern int unpoison_memory(unsigned long pfn);
+extern void collect_procs_and_signal_inode(struct inode *inode, pgoff_t pgoff,
+				    unsigned long pfn, int flags);
 extern int get_hwpoison_page(struct page *page);
 #define put_hwpoison_page(page)	put_page(page)
 extern int sysctl_memory_failure_early_kill;
