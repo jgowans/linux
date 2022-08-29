ec4d65c109b2616539ea0151084fdd33ef82f8b3
James Gowans <jgowans@amazon.com>
2022-08-29 19:01:29 +0200

    mmuse: mountable filesystem
    Does nothing other than seems to mostly be able to be mounted.
    
    root@debian:~# mount -t mmuse mmuse /mnt/mmuse/
    [    7.638738] Would init fs context here
    [    7.638992] setting (and leaking? root inode and dentry
    
    root@debian:~# mount | grep muse
    mmuse on /mnt/mmuse type mmuse (rw,relatime)
    
    root@debian:~# ls -alh /mnt/mmuse/
    total 4.0K
    drwxr-xr-x 2 root root    0 Aug 31 10:18 .
    drwxr-xr-x 3 root root 4.0K Aug 28 21:01 ..
    
    Signed-off-by: James Gowans <jgowans@amazon.com>

---
+++ fs/mmuse/mmuse.c
@@ -0,0 +1,69 @@
+/* SPDX-License-Identifier: MIT */
+
+#include <linux/dcache.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/fs_context.h>
+
+static const struct super_operations mmuse_super_ops = { };
+
+static int mmuse_fill_super(struct super_block *sb, struct fs_context *fc)
+{
+	struct inode *inode;
+	struct dentry *dentry;
+
+	sb->s_op = &mmuse_super_ops;
+
+	inode = new_inode(sb);
+	if (!inode)
+		return -ENOMEM;
+
+	inode->i_ino = get_next_ino();
+	inode->i_mode = S_IFDIR;
+	inode->i_op = &simple_dir_inode_operations;
+	inode->i_fop = &simple_dir_operations;
+	inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
+	/* directory inodes start off with i_nlink == 2 (for "." entry) */
+	inc_nlink(inode);
+
+	dentry = d_make_root(inode);
+	if (!dentry)
+		return -ENOMEM;
+	sb->s_root = dentry;
+
+	return 0;
+}
+
+static int mmuse_get_tree(struct fs_context *fc)
+{
+	return get_tree_nodev(fc, mmuse_fill_super);
+}
+
+static const struct fs_context_operations mmuse_context_ops = {
+	.get_tree	= mmuse_get_tree,
+};
+
+static int mmuse_init_fs_context(struct fs_context *const fc)
+{
+	fc->ops = &mmuse_context_ops;
+	return 0;
+}
+
+static struct file_system_type mmuse_fs_type = {
+	.owner                  = THIS_MODULE,
+	.name                   = "mmuse",
+	.init_fs_context        = mmuse_init_fs_context,
+	.kill_sb                = kill_litter_super,
+	.fs_flags               = FS_USERNS_MOUNT,
+};
+
+static int __init mmuse_init(void)
+{
+	int ret;
+	ret = register_filesystem(&mmuse_fs_type);
+	printk("mmuse_init: %i\n", ret);
+	return ret;
+}
+
+MODULE_ALIAS_FS("mmuse");
+module_init(mmuse_init);
