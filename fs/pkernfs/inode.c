2476208b0e88ad799bb41a49ea739e4d70cb9e57
James Gowans <jgowans@amazon.com>
2023-08-24 13:30:48 +0200

    pkernfs: Start at allocatable inode
    Signed-off-by: James Gowans <jgowans@amazon.com>

--- fs/pkernfs/inode.c
+++ fs/pkernfs/inode.c
@@ -5,6 +5,10 @@
 
 const struct inode_operations pkernfs_dir_inode_operations;
 
+static struct pkernfs_inode *pkernfs_get_persisted_inode(struct super_block *sb, int ino) {
+	return ((struct pkernfs_inode *) (pkernfs_mem + (2 << 20))) + ino;
+}
+
 struct inode *pkernfs_inode_get(struct super_block *sb, unsigned long ino) {
 	struct inode *inode;
 	inode = iget_locked(sb, ino);
@@ -21,7 +25,17 @@ struct inode *pkernfs_inode_get(struct super_block *sb, unsigned long ino) {
 	return inode;
 }
 
-
+static int pkernfs_get_next_free_inode_no(struct super_block *sb)
+{
+	/* Inode store is 2nd 2 MiB page */
+	struct pkernfs_inode *inode_store_mem = (struct pkernfs_inode *) (pkernfs_mem + (2 << 20));
+	for (int inode_idx = 0; (inode_idx < ((2 << 20)) / sizeof(struct pkernfs_inode)); inode_idx++) {
+	    if (!inode_store_mem[inode_idx].flags) {
+		return inode_idx;
+	    }
+	}
+	return -ENOMEM;
+}
 
 void pkernfs_zero_inode_store(struct super_block *sb)
 {
@@ -35,7 +49,20 @@ static int pkernfs_create(struct mnt_idmap *id,
 		struct dentry *dentry,
 		umode_t mode,
 		bool excl) {
-	printk("pkernfs_create invoked\n");
+	int free_inode;
+	struct pkernfs_inode *pkernfs_inode;
+	struct inode *vfs_inode;
+	printk("pkernfs_create invoked for %s\n", dentry->d_name.name);
+
+	free_inode = pkernfs_get_next_free_inode_no(dir->i_sb);
+	if (free_inode < 0)
+	    return free_inode;
+
+	vfs_inode = pkernfs_inode_get(dir->i_sb, free_inode);
+	pkernfs_inode = pkernfs_get_persisted_inode(dir->i_sb, free_inode);
+	strncpy(pkernfs_inode->filename, dentry->d_name.name, 32);
+
+	printk("next free inode: %i\n", free_inode);
 	return 0;
 }
 
@@ -43,7 +70,7 @@ static struct dentry *pkernfs_lookup(struct inode *dir,
 		struct dentry *dentry,
 		unsigned int flags)
 {
-	printk("pkernfs_lookup invoked\n");
+	printk("pkernfs_lookup invoked for %s\n", dentry->d_name.name);
 	return NULL;
 }
 
